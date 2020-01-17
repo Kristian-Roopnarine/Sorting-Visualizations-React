9d7e5712fd60a610b0bb7ed518609a96ef63f664
Kristian-Roopnarine <48737383+Kristian-Roopnarine@users.noreply.github.com>
2020-01-17 18:41:34 -0500

    Add files via upload

--- src/SortingVisualizer/SortingVisualizer.js
+++ src/SortingVisualizer/SortingVisualizer.js
@@ -1,5 +1,11 @@
 import React from 'react'
 import "./SortingVisualizer.css"
+import quickSort from "./quicksort.js"
+
+const DEFAULT_COLOR = 'blue'
+const PIVOT_COLOR = 'red'
+const COMPARISON_COLOR = 'yellow'
+const MOVING_PIVOT = '#A3E4D7'
 
 export default class SortingVisualizer extends React.Component{
     constructor(props){
@@ -10,10 +16,59 @@ export default class SortingVisualizer extends React.Component{
         // The array will be filled with 200 bars, of random values.
         // Need to setState after array is populated.
         this.state = {
-            array : []
+            array : [],
+            animations :[],
+            sorted: false,
         }
         // binds the onclick to a variable
-        this.createNewArray=this.createNewArray.bind(this)
+        this.createNewArray = this.createNewArray.bind(this)
+        this.startQuickSort = this.startQuickSort.bind(this)
+    }   
+
+    //this should sort the current array
+    //it then updates the state to re render the page
+    startQuickSort(){
+        if (this.state.sorted == false){
+            let sortedArray = quickSort(this.state.array,0,this.state.array.length-1,this.state.animations)
+            for (let i = 0;i < sortedArray.length-1;i++){
+                const arrayBar = document.getElementsByClassName('array-bar');
+                const [pivotIndex,pivotValueIndex,comparison,swap] = sortedArray[i];
+                const pivotValue = arrayBar[pivotValueIndex];
+                const comparisonValue = arrayBar[comparison];
+                const pivotIndexValue = arrayBar[pivotIndex];
+
+                setTimeout(()=>{
+                    // Changes bar colors to correct color
+                    comparisonValue.style.backgroundColor = COMPARISON_COLOR;
+                    pivotIndexValue.style.backgroundColor = MOVING_PIVOT;
+
+                    if (swap === 1){
+                        // if swapping pivot index and comparison (if arr[i] < pivotValue)
+                        const barOneHeight = pivotIndexValue.style.height;
+                        const barTwoHeight = comparisonValue.style.height;
+                        pivotIndexValue.style.height = `${barTwoHeight}`;
+                        comparisonValue.style.height = `${barOneHeight}`;
+    
+                    } else if (swap === 2){
+                        // swap pivot value and pivot index (ending for loop in partition)
+                        const barOneHeight = pivotValue.style.height;
+                        const barTwoHeight = pivotIndexValue.style.height;
+                        pivotValue.style.height = `${barTwoHeight}`;
+                        pivotIndexValue.style.height = `${barOneHeight}`;
+                    }
+                    setTimeout(()=>{
+                        //change the bars back to original color
+                        comparisonValue.style.backgroundColor = DEFAULT_COLOR;
+                        pivotIndexValue.style.backgroundColor = DEFAULT_COLOR;
+                    },50);
+                },i*1.5);
+            }
+            
+        this.setState({sorted:true});
+        } else {
+            return; 
+        }
+        
     }
     // when the page loads and DOM is inserted then the array will be populated.
     componentDidMount(){
@@ -30,7 +85,8 @@ export default class SortingVisualizer extends React.Component{
         for (let i = 0; i < 300 ; i++){
             array.push(randomIntFromInterval(5,400))
         };
-        this.setState({array:array});
+        this.setState({array:array,animations:[]});
+        this.setState({sorted:false})
     }
 
     render() {
@@ -38,10 +94,10 @@ export default class SortingVisualizer extends React.Component{
         //turn each of the array elements into a dom element
         return (
             <div className="array-container">
-                {array.map((value,idx) => <div className="array-bar" key={idx} style={{height:`${value}px`}}></div>)}
+                {array.map((value,idx) => <div className="array-bar" key={idx} style={{height:`${value}px`,backgroundColor:DEFAULT_COLOR}}></div>)}
                 <div>
-                    <button class="qs-button" type="Submit">Start Quicksort!</button>
-                    <button class="qs-button" type="Submit" onClick={this.createNewArray}>Create New Array!</button>
+                    <button className="qs-button" type="Submit" onClick ={this.startQuickSort}>Start Quicksort!</button>
+                    <button className="qs-button" type="Submit" onClick={this.createNewArray}>Create New Array!</button>
                 </div>
                 
             </div> 
@@ -54,3 +110,10 @@ export default class SortingVisualizer extends React.Component{
 function randomIntFromInterval(min, max) { // min and max included 
     return Math.floor(Math.random() * (max - min + 1) + min);
   }
+
+function swap2(a,b){
+    let temp = a;
+    a = b;
+    b = temp;
+    return a,b
+}
--- src/SortingVisualizer/quicksort.js
+++ src/SortingVisualizer/quicksort.js
@@ -1,22 +1,39 @@
-function quickSort(arr,start,end){
+export default function quickSort(arr,start,end,animations){
+    const middleArray = arr.slice()
+    quickSort2(middleArray,0,middleArray.length-1,animations)
+    return animations
+}
+
+function quickSort2(arr,start,end,animations){
     if (start >= end) {
         return;
     }
-    let index = partition(arr,start,end);
-    quickSort(arr,start,index)
-    quickSort(arr,index+1,end)
+    let index = partition(arr,start,end,animations);
+    quickSort2(arr,start,index-1,animations);
+    quickSort2(arr,index+1,end,animations);
 }
 
-function partition(arr,start,end){
+function partition(arr,start,end,animations){
     let pivotIndex = start;
     let pivotValue = arr[end];
     for (let i = start ; i < end;i++){
+
+        //end is the current pivotIndexValue
+        //i returns the index of element being compared.
+        // 0 if being compared, 1 if comparison and pivot are being swapped
+        // 2 if pivotindex and end are being swapped
+        animations.push([pivotIndex,end,i,0])
+        animations.push([pivotIndex,end,i,0])
         if (arr[i] < pivotValue){
             swap(arr,i,pivotIndex);
+            animations.push([pivotIndex,end,i,1]);
             pivotIndex++;
+        } else {
+            animations.push([pivotIndex,end,i,0])
         }
     }
     swap(arr,pivotIndex,end);
+    animations.push([pivotIndex,end,0,2])
     return pivotIndex;
 }
 
